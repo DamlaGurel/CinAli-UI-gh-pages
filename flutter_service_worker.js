7c9aae13a6ac3508002165ef5d5d7a110c2d487f
damlagurel <damla.gureel@gmail.com>
2025-10-10 14:59:37 +0300

    null hatası ve varyanta resim ekleme eklendi.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f753e2609e0ce2275a8d892845b02069",
+const RESOURCES = {"flutter_bootstrap.js": "1b991889a0ef1f73da094d18a7dca36a",
 "version.json": "be1d7114dd59e8d46fea116a9a8d8940",
 "index.html": "624f7150dbb1f4be7b2aa8aa66f093f4",
 "/": "624f7150dbb1f4be7b2aa8aa66f093f4",
-"main.dart.js": "9db3a71ff7aecc57a6e2aa2d8968ade8",
+"main.dart.js": "603f3203b19dcd6770df68ed85c53fd5",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
