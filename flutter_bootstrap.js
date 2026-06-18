d43bb189521b8e25f02501cc5295fd5e37733c1e
Emin Soysal <emin.m.soysal@gmail.com>
2026-06-18 10:29:38 +0300

    Deploy Flutter Web with correct base href (CinAli-UI@918e26b (dirty) — 2026-06-18 07:29:38 UTC)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1548141139" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3585274630" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
